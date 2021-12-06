e91b2af59d00c1060d720a1760012bc30c6cdcfe
bruna <busouza01@gmail.com>
2021-12-06 08:09:59 -0500

    componente dos stories criado

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,60 +1,9 @@
+import Stories from "./Stories";
 export default function Contents() {
     return (
         <div class="container">
             <div class="timeline">
-                <ul class="stories">
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/ig-11.jpg">
-                        </div>
-                        <p>ig-11</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/bb-8.jpg">
-                        </div>
-                        <p>bb-8</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/ahsoka-tano (2).jpg">
-                        </div>
-                        <p>ahsoka_tano</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/r2-d2.jpg">
-                        </div>
-                        <p>r2-d2</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/The-Mandalorian-Kuiil-Vest-510x600.jpg">
-                        </div>
-                        <p>Kuiil</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/c-d.jpg">
-                        </div>
-                        <p>cara.dune</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/grogu1.jpg">
-                        </div>
-                        <p>grogu</p>
-                    </li>
-                    <li class="storie">
-                        <div class="imagem-storie">
-                            <img src="img/ue.jpg">
-                        </div>
-                        <p>semfuturo</p>
-                    </li>
-                    <div class="botao-scroll">
-                        <ion-icon name="chevron-forward-sharp"></ion-icon>
-                    </div>
-                </ul>
+                <Stories />
 
                 <div class="post">
                     <div class="post-topo">
---
+++ src/Components/Stories.js
@@ -0,0 +1,57 @@
+export default function Stories () {
+    return (
+        <ul class="stories">
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/ig-11.jpg" />
+                </div>
+                <p>ig-11</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/bb-8.jpg" />
+                </div>
+                <p>bb-8</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/ahsoka-tano (2).jpg" /> 
+                </div>
+                <p>ahsoka_tano</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/r2-d2.jpg" /> 
+                </div>
+                <p>r2-d2</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/The-Mandalorian-Kuiil-Vest-510x600.jpg" />
+                </div>
+                <p>Kuiil</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/c-d.jpg" />
+                </div>
+                <p>cara.dune</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/grogu1.jpg" />
+                </div>
+                <p>grogu</p>
+            </li>
+            <li class="storie">
+                <div class="imagem-storie">
+                    <img src="img/ue.jpg" />
+                </div>
+                <p>semfuturo</p>
+            </li>
+            <div class="botao-scroll">
+                <ion-icon name="chevron-forward-sharp"></ion-icon>
+            </div>
+        </ul>
+    );
+}
